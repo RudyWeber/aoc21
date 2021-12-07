e4bace0ff17a5ffadf89c6cccd75172e0a5ac04e
Rudy Weber <webrudy@gmail.com>
2021-12-07 13:22:04 +0100

    more utils

--- utils/aoc.ts
+++ utils/aoc.ts
@@ -1,15 +1,22 @@
 const BASE_URL = 'https://adventofcode.com/2021/day/';
-const AOC_SESSION = Deno.env.get('AOC_SESSION');
-
-if (!AOC_SESSION) {
-  console.log('[!] AOC_SESSION env variable missing.');
-  Deno.exit(-1);
-}
 
 export const getInput = async (dayNumber: number): Promise<string> => {
+  const AOC_SESSION = Deno.env.get('AOC_SESSION');
+
+  if (!AOC_SESSION) {
+    console.log('[!] AOC_SESSION env variable missing.');
+    Deno.exit(-1);
+  }
+
   const headers = new Headers({
     cookie: `session=${AOC_SESSION}`
   });
   const response = await fetch(`${BASE_URL}${dayNumber}/input`, { headers });
   return response.text();
 }
+
+export const getInputFromFile = async (filename: string): Promise<string> => {
+  const file = await Deno.readFile(filename);
+  const decoder = new TextDecoder('utf-8');
+  return decoder.decode(file);
+};
