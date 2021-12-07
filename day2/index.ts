75f27c10b132717036fd725d158e6f357e68a86b
Rudy Weber <webrudy@gmail.com>
2021-12-07 13:22:38 +0100

    day 2.1

---
+++ day2/index.ts
@@ -0,0 +1,37 @@
+import { getInputFromFile } from '../utils/aoc.ts';
+import { pipe } from '../utils/fp.ts';
+
+type command = 'forward' | 'up' | 'down';
+
+const input = await getInputFromFile('./input');
+
+const parseInput = (input: string): [cmd: command, n: number][] =>
+  input.split('\n').filter(Boolean).map(entry => {
+    const splitEntry = entry.split(' ');
+    return [splitEntry[0] as command, +splitEntry[1]];
+  })
+
+const calculatePosition = (xs: [cmd: command, n:number][]): {horizontal: number, depth: number} =>
+  xs.reduce<{horizontal: number, depth: number}>(({ horizontal, depth }, [cmd, n]) => {
+    switch (cmd) {
+      case 'forward':
+        return { horizontal: horizontal + n, depth };
+      case 'down':
+        return { horizontal, depth: depth + n };
+      case 'up':
+        return { horizontal, depth: depth - n };
+    }
+  }, { horizontal: 0, depth: 0 });
+
+const calculateScore = ({ horizontal, depth }: { horizontal: number, depth: number }): number => horizontal * depth;
+
+const solve1: (input: string) => number = 
+  pipe(
+    parseInput,
+    calculatePosition,
+    calculateScore
+  );
+
+const result1 = solve1(input);
+
+console.log(result1);
