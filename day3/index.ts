5616a62bcc97c938235b025ac5f6cd13dad10c83
Rudy Weber <webrudy@gmail.com>
2021-12-07 23:03:31 +0100

    Day 3.2

--- day3/index.ts
+++ day3/index.ts
@@ -24,16 +24,63 @@ const calculateGammaAndEpsilonRates = (
   return { gamma, epsilon };
 };
 
-const calculateScore = (
+const calculateOxygenRating = (xs: string[], index = 0): string[] => {
+  const onlyNthBit = xs.map((x) => +x[index]);
+  const { zeros, ones } = onlyNthBit.reduce(({ zeros, ones }, bit) => ({
+    zeros: bit === 0 ? zeros + 1 : zeros,
+    ones: bit === 1 ? ones + 1 : ones,
+  }), { zeros: 0, ones: 0 });
+  const bitToKeep = zeros > ones ? 0 : 1;
+  const kept = xs.filter((x) => +x[index] === bitToKeep);
+
+  if (kept.length === 1) {
+    return kept;
+  }
+
+  return calculateOxygenRating(kept, index + 1);
+};
+
+const calculateCO2Rating = (xs: string[], index = 0): string[] => {
+  const onlyNthBit = xs.map((x) => +x[index]);
+  const { zeros, ones } = onlyNthBit.reduce(({ zeros, ones }, bit) => ({
+    zeros: bit === 0 ? zeros + 1 : zeros,
+    ones: bit === 1 ? ones + 1 : ones,
+  }), { zeros: 0, ones: 0 });
+  const bitToKeep = zeros <= ones ? 0 : 1;
+  const kept = xs.filter((x) => +x[index] === bitToKeep);
+
+  if (kept.length === 1) {
+    return kept;
+  }
+
+  return calculateCO2Rating(kept, index + 1);
+};
+
+const calculateScore1 = (
   { gamma, epsilon }: { gamma: number; epsilon: number },
 ): number => gamma * epsilon;
+
+const calculateScore2 = (
+  { oxigen, co2 }: { oxigen: number; co2: number },
+) => oxigen * co2;
 
 const solve1: (input: string) => number = pipe(
   parseInput,
   calculateGammaAndEpsilonRates,
-  calculateScore,
+  calculateScore1,
+);
+
+const solve2: (input: string) => number = pipe(
+  parseInput,
+  (x) => ({
+    oxigen: Number.parseInt(calculateOxygenRating(x)[0], 2),
+    co2: Number.parseInt(calculateCO2Rating(x)[0], 2),
+  }),
+  calculateScore2,
 );
 
 const result1 = solve1(input);
+const result2 = solve2(input);
 
 console.log(result1);
+console.log(result2);
